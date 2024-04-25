c7b5ee2acf24250292d86a077f27fa312f544faa
Evgeniya Makeeva <makeevaeva91@gmail.com>
2024-04-25 18:07:11 +0300

    updated file

--- index.js
+++ index.js
@@ -11,14 +11,14 @@ document.querySelector('.card__input-name').oninput = () =>{
 
 
     const nameInput = document.querySelector('.card__input-name');
-    const nameOutput = document.querySelector('.card__name-input')
+    const nameOutput = document.querySelector('.card__name-input');
     const nameValue = nameInput.value
 
     if(/^[a-zA-Zа-яА-я\s]+$/.test(nameValue)){
        nameOutput.innerHTML = nameValue
     }else{
      nameInput.innerHTML = '';   
-     nameOutput.innerHTML = 'Invalid name'
+     nameOutput.innerHTML = 'Invalid name';
     }
 }
 
@@ -27,7 +27,7 @@ document.querySelector('.card__input-month').oninput = () =>{
 }
 
 const expirationSelect = document.querySelector("[data-expiration-year]");
-const logo = document.querySelector('[data-logo]')
+const logo = document.querySelector('[data-logo]');
 
 const currentYear = new Date().getFullYear();
 for(let i = currentYear; i < currentYear + 10; i++){
@@ -66,98 +66,106 @@ document.querySelector('.card__input-cvv').oninput = () =>{
 
 
 document.addEventListener('keydown', e => { 
-    const input = e.target
-    const key = e.key
-    if(!isConnectedInput(input)) return
+    const input = e.target;
+    const key = e.key;
+    if(!isConnectedInput(input)) return;
 
     switch (key){
         case "ArrowLeft": {
             if(input.selectionStart === 0 && input.selectionEnd === 0){
-               const prev = input.previousElementSibling
-               prev.focus()
-               prev.selectionStart = prev.value.length - 1
-               prev.selectionEnd = prev.value.length - 1
+               const prev = input.previousElementSibling;
+               if(!prev) break;
+               prev.focus();
+               prev.selectionStart = prev.value.length - 1;
+               prev.selectionEnd = prev.value.length - 1;
                e.preventDefault()
             }
             break
         }  
         case "ArrowRight": {
             if(input.selectionStart === input.value.length && input.selectionEnd === input.value.length){
-               const next = input.nextElementSibling
-               next.focus()
-               next.selectionStart = 1
-               next.selectionEnd = 1
-               e.preventDefault()
+               const next = input.nextElementSibling;
+               if(!next) break;
+               next.focus();
+               next.selectionStart = 1;
+               next.selectionEnd = 1;
+               e.preventDefault();
             }
             break
         } 
         case "Delete": {
             if(input.selectionStart === input.value.length && input.selectionEnd === input.value.length){
-                const next = input.nextElementSibling
-                next.value = next.value.substring(1, next.value.length)
-                next.focus()
-                next.selectionStart = 0
-                next.selectionEnd = 0
-                e.preventDefault()
+                const next = input.nextElementSibling;
+                next.value = next.value.substring(1, next.value.length);
+                next.focus();
+                next.selectionStart = 0;
+                next.selectionEnd = 0;
+                e.preventDefault();
              }
              break 
         } 
         case "Backspace": {
             if(input.selectionStart === 0 && input.selectionEnd === 0){
-                const prev = input.previousElementSibling
-                prev.value = prev.value.substring(0, prev.value.length - 1)
-                prev.focus()
-                prev.selectionStart = prev.value.length
-                prev.selectionEnd = prev.value.length
-                e.preventDefault()
+                const prev = input.previousElementSibling;
+                prev.value = prev.value.substring(0, prev.value.length - 1);
+                prev.focus();
+                prev.selectionStart = prev.value.length;
+                prev.selectionEnd = prev.value.length;
+                e.preventDefault();
              }
              break
         }
         default: { 
-          if(key.match(/^[^0-9]+$/)) return e.preventDefault();
+          if (key.match(/\d/)) onInputChange(input, key);
+          e.preventDefault();
 
-          onInputChange(input, key)
-    }}
+          updateCardNumbers();        
+    }
+}
 });
+
+function updateCardNumbers(){
+    cardNumbers.forEach((card, index) => (card.innerHTML = inputNumbers[index].value.padEnd(4, '#')));
+}
 
 function onInputChange(input, newValue){
   const start = input.selectionStart;
   const end = input.selectionEnd;  
-  updateInputValue(input, newValue, start, end)
-  focusInput(input, newValue.length + start)
+  updateInputValue(input, newValue, start, end);
+  focusInput(input, newValue.length + start);
   const firstFourDigits = input.closest('[data-connected-inputs]').querySelector('input').value;
 
   if(firstFourDigits.startsWith('4')){
-    logo.src = 'img/visa.png'
+    logo.src = 'img/visa.png';
   } else if (firstFourDigits.startsWith('5')){
-    logo.src = 'img/mastercard.png'
+    logo.src = 'img/mastercard.png';
   } else if(firstFourDigits.startsWith('6')){
-    logo.src = 'img/unionPay.png'
+    logo.src = 'img/unionPay.png';
   }
 }
 
 function updateInputValue(input, extraValue, start = 0, end = 0){
     const newValue = `${input.value.substring(0, start)}${extraValue}${input.value.substring(end, input.value.length)}`
-    input.value = newValue.substring(0, 4)
+    input.value = newValue.substring(0, 4);
     if(newValue > 4){
-        const next = input.nextElementSibling
-        if(next == null) return
-        updateInputValue(next, newValue.substring(4))
+        const next = input.nextElementSibling;
+        if(next == null) return;
+        updateInputValue(next, newValue.substring(4));
     }
 }
 
 function focusInput(input, dataLength){
-    let addedCharacters = dataLength
-    let currentInput = input
+    let addedCharacters = dataLength;
+    let currentInput = input;
     while (addedCharacters > 4 && currentInput.nextElementSibling != null){
-        addedCharacters -= 4
-        currentInput = currentInput.nextElementSibling
+        addedCharacters -= 4;
+        currentInput = currentInput.nextElementSibling;
     }
-    if(addedCharacters > 4) addedCharacters = 4
+    if(addedCharacters > 4) addedCharacters = 4;
 
     currentInput.focus()
-    currentInput.selectionStart = addedCharacters
-    currentInput.selectionEnd = addedCharacters
+    currentInput.selectionStart = addedCharacters;
+    currentInput.selectionEnd = addedCharacters;
 }
 
 function isConnectedInput(input){
